7ae6af3c1f1336ab0bdb482320f3f51d3ea211d2
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-12 22:52:04 -0600

    take away css sheets and entered header and title

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <!DOCTYPE>
-<html>
+<html lang="en">
+		<head>
+				<meta charset="utf-8" />
+
+				<title>A Little Bit About Me</title>
+		</head>
+		<body>
+				<!-- Enter header here -->
+				<header>
+							<h1>Husband, Father, Runner, and Future Devloper</h1>
+				</header>
+
+				<!-- Start main content here -->
+				<main>
+
+				</main>
+		</body>
 	
 </html>
