467df0a6fec2599bedab249d7323cf841c7939a2
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-14 23:26:30 -0600

    Added name in header and placed intro in main section

--- index.php
+++ index.php
@@ -1,20 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
 		<head>
-				<meta charset="utf-8" />
+			<meta charset="utf-8" />
+			
+			<!-- Adding custom CSS -->
+			<link href="css/stylesheet.css" rel="stylesheet" type"text/css" />
+			
+			<!-- Adding custom JavaScript-->
+			<script src="js/custom.js" type="text/javascript"></script>
 
-				<title>A Little Bit About Me</title>
+			<title>A Little Bit About Me</title>
 		</head>
 		<body>
-				<!-- Enter header here -->
-				<header>
-							<h1>Husband, Father, Runner, and Future Devloper</h1>
-				</header>
 
-				<!-- Start main content here -->
-				<main>
+			<!-- This should put the body in a container -->
+			<div class="container">
 
-				</main>
+			<!-- Enter header here -->
+			<header>
+				<h1>My name is Giles Sandoval</h1>
+				<p>A Little Bit About Me</p>
+			</header>
+
+			<!-- Start main content here -->
+			<main>
+				<p>I grew up in San Felipe Pueblo in a big family. I'm the oldest of many brothers, sisters, and close cousins. I went to high school, college, and have had many fun jobs prior to this class. I've been a chef, a paramedic, a store manager, a director of a team, a mentor, and a coach. I've had other important jobs, such as working as a janitor, busser, and dishwasher. Those jobs taught me the importance of hard work. Here's where I'm at now...</p>
+				<h2>Husband, Father, Runner, and Future Developer</h2>
+				<h3>Married on August 22, 2015</h3>
+				<img class="flt-center" src="images/bandg.jpg" alt="marriage photo" />
+
+				<!-- Placing an unordered list here -->
+				<ul>
+					<li>I'm abundantly blessed to be married to this beautiful, wise, and mighty woman of God.</li>
+					<li>She is the wonderful mother of our handsome baby boy.</li>
+					<li>She is my personal trainer, both physically and mentally.</li>
+				</ul>
+
+				<!-- Father part -->
+				<h3>We officially became parents on May 21, 2016 @ 2207 Hours</h3>
+				<!-- Find photo and delete this line -->
+				<img src="images/grey.jpeg" alt="baby photo" />
+
+				<!-- Placing unordered list -->
+				<ul>
+					<li>He was 19.75 inches; 7 pounds and 2.8 ounces.</li>
+					<li>He loves music, drinking milk, sleeping, going potty, and more snoozing.</li>
+					<li>Current fluent language: Speaks "Baby"; Future languages: English, Keres (my native language), French, and Coding (hopefully).</li>
+				</ul>
+
+				<!-- Runner part -->
+				<h3>I ran my first marathon in October 2013</h3>
+				<img src="images/runner.jpg" alt="runner photo" />
+
+				<!-- Placing unordered list-->
+				<ul>
+					<li>I ran the Duke City Marathon of 26.2 miles.</li>
+					<li>Technically, I ran 27.3 miles, I couldn't find the starting line! :)</li>
+					<li>"Your body will argue that there is no justifiable reason to continue. Your only recourse is to call on your spirit, which fortunately functions independently of logic." -Tim Noakes</li>
+				</ul>
+
+				<!-- Developer part-->
+				<!-- STILL NEED TO ADD IMAGE HERE -->
+				<h3>Future Web Developer. Why?</h3>
+				<img class="flt-center" src="images/awesomeidea.jpg" alt="developer photo" />
+
+				<!-- Placing unordered list-->
+				<ul>
+					<li>It's a challenge. It requires patience, teamwork, and persistence. I'm ready, bring it on.</li>
+					<li>Having an idea and then using it to change the world. That's what I hope to do in this field.</li>
+					<li>Because "software is about humanity. It's about helping people." -Someone smarter than me :)</li>
+				</ul>
+
+				<!-- Consider inserting a link here -->
+				<p>For an inspiring message from the creators of Valve, facebook, Microsoft, Clothia.com, and Twitter...Click the link below. </p>
+				<div>
+					<a href="https://www.youtube.com/watch?v=OWsyrnOBsJs" target="_blank">Computer Programming | A short interesting film</a>
+				</div>
+				
+				<!-- Entered this for Space -->
+				<p></p>
+			</main>
+
+			<!-- Begin footer page -->
+			<footer>
+				&copy; 2016
+			</footer>
 		</body>
-	
 </html>
